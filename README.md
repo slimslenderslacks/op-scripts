81915f9ddecdb64ee47b011e644769c67dbe6887
Jim Clark <slimslenderslacks@gmail.com>
2020-01-15 21:08:22 -0800

    publish script

---
+++ README.md
@@ -0,0 +1,58 @@
+## Background
+
+Extract environment variables from 1password.   
+
+## Installation
+
+[Install the 1password cli](https://support.1password.com/command-line-getting-started/)
+
+```
+npm install -g @slimslenderslacks/op-scripts
+```
+
+You should be able to signin to 1password using `op signin --help` and after the first time you've successfully logged in, 
+there's an easier form that you can use to setup a 1password session in the current environmet:
+
+```
+eval $(op signin atomist)
+```
+
+As long as you're logged in, you can `export_secrets` will write stdout `export` statements with data from 1password
+
+```
+20:49 $ export_secrets
+export CLOJARS_USERNAME=xxxxx
+export CLOJARS_PASSWORD=xxxxxxxx
+```
+
+So you can use `eval $(export_secrets)` to setup a local bash environment with some secret data.
+
+## Configuration
+
+You can map data from 1password into the `export` statements using an `.edn` file at `~/.export-1password`.
+
+```
+{"Clojars" {"CLOJARS_USERNAME" "username"
+            "CLOJARS_PASSWORD" "password"}
+ "document/githuboauthredirect.json" {"GITHUBOAUTHREDIRECT_PASSPHRASE" [:data :passphrase]
+                                      "GITHUBOAUTHREDIRECT_PEM" [:data :pem]}}
+```
+
+The keys are named items in the 1password atomist "Team" vault.  The values are maps that setup the `export` statements 
+(keys are the name of the environment variables, and the values are fields in the 1password secrets).
+
+### jwt-creator
+
+The jwt-creator can be started with a ~/.export-1password of
+
+```
+{"document/githuboauthredirect.json" {"GITHUBOAUTHREDIRECT_PASSPHRASE" [:data :passphrase]
+                                      "GITHUBOAUTHREDIRECT_PEM" [:data :pem]}}
+```
+
+and then
+
+```
+eval $(op signin atomist)
+eval $(export_secrets)
+```
--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -5,36 +5,53 @@
    [cljs.nodejs.shell :as sh]
    [goog.string :as gstring]
    [goog.string.format]
+   [cljs.reader]
    [cljs.pprint :refer [pprint]]))
 
 (enable-console-print!)
 
-(def export-1password {"Clojars" {"CLOJARS_USERNAME" "username"
-                                  "CLOJARS_PASSWORD" "password"}
-                       "ApiKeySlimslenderslacksProd" {"API_KEY_SLIMSLENDERSLACKS_PROD" "password"}
-                       "ApiKeySlimslenderslacksStaging" {"API_KEY_SLIMSLENDERSLACKS_STAGING" "password"}
-                       "GitHubTokenSlimSlenderSlacks" {"GITHUB_TOKEN" "password"}})
+(defn- op-mappings []
+  (cljs.reader/read-string (slurp (gstring/format "%s/.export-1password" (.. js/process -env -HOME)))))
+
+(defn- document-handler [mapping out err]
+  (let [item (js->clj (.parse js/JSON out) :keywordize-keys true)]
+    (doseq [[env-name path] (seq mapping)]
+      (println (gstring/format "export %s=%s" env-name (get-in item path))))))
+
+(defn- item-handler [mapping out err]
+  (let [item (js->clj (.parse js/JSON out) :keywordize-keys true)]
+    (doseq [[env-name designation] (seq mapping)]
+      (println (gstring/format "export %s=%s" env-name (or
+                                                        (->> item :details :fields (filter #(= designation (:designation %))) first :value)
+                                                        (if (= "password" designation) (->> item :details :password))))))))
+
+(defn- op-handler [out err callback]
+  (cond
+    (s/includes? err "401: Authentication required.")
+    (throw (ex-info "Authentication required." {:error :auth
+                                                :message "Authentication required:  please run `eval $(op signin atomist)` and then retry"}))
+
+    (s/includes? err "You are not currently signed in. Please run `op signin --help` for instructions")
+    (throw (ex-info err {:error :auth
+                         :message "You are not signed in:  please run `eval $(op signin atomist)` and then retry"}))
+
+    :else
+    (callback out err)))
 
 (defn ^:export handler []
   (try
-    (let [args (drop 2 (. js/process -argv))]
+    (let [args (drop 2 (. js/process -argv))
+          export-1password (op-mappings)]
       (doseq [[k v] (seq export-1password)]
-        (let [{:keys [out err]} (sh/sh "op" "get" "item" k)]
-          (cond
-            (s/includes? err "401: Authentication required.")
-            (throw (ex-info "Authentication required." {:error :auth
-                                                        :message "Authentication required:  please run `eval $(op signin atomist)` and then retry"}))
+        (cond
 
-            (s/includes? err "You are not currently signed in. Please run `op signin --help` for instructions")
-            (throw (ex-info err {:error :auth
-                                 :message "You are not signed in:  please run `eval $(op signin atomist)` and then retry"}))
+          (s/starts-with? k "document")
+          (let [{:keys [out err]} (sh/sh "op" "get" "document" (s/replace k #"document/" ""))]
+            (op-handler out err (partial document-handler (get export-1password k))))
 
-            :else
-            (let [item (js->clj (.parse js/JSON out) :keywordize-keys true)]
-              (doseq [[env-name designation] (seq (get export-1password k))]
-                (println (gstring/format "export %s=%s" env-name (or
-                                                                  (->> item :details :fields (filter #(= designation (:designation %))) first :value)
-                                                                  (if (= "password" designation) (->> item :details :password)))))))))))
+          :else
+          (let [{:keys [out err]} (sh/sh "op" "get" "item" k)]
+            (op-handler out err (partial item-handler (get export-1password k)))))))
     (.exit js/process 0)
     (catch :default ex
       (println "error" (ex-message ex))
